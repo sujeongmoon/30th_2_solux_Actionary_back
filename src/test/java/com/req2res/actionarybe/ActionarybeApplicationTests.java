465de49182e203898d1c264fb287ea3ac5352d6b
sujeongmoon <msjk9000@gmail.com>
2025-11-05 16:59:30 +0900

    feat: #1 - springboot 초기설정

---
+++ src/test/java/com/req2res/actionarybe/ActionarybeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.req2res.actionarybe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActionarybeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
