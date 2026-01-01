8b06da356d608c605019c12ad78110d7cf11341b
NaHyeon647 <forzeu@sookmyung.ac.kr>
2026-01-01 14:41:45 +0900

    feat:#52 - create comment & get popluar/latest post success

--- src/main/java/com/req2res/actionarybe/domain/comment/controller/CommentController.java
+++ src/main/java/com/req2res/actionarybe/domain/comment/controller/CommentController.java
@@ -1,4 +1,30 @@
 package com.req2res.actionarybe.domain.comment.controller;
 
+import com.req2res.actionarybe.domain.comment.dto.CreateCommentRequestDTO;
+import com.req2res.actionarybe.domain.comment.dto.CreateCommentResponseDTO;
+import com.req2res.actionarybe.domain.comment.service.CommentService;
+import com.req2res.actionarybe.global.Response;
+import com.req2res.actionarybe.global.security.CustomUserDetails;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/posts")
+@RequiredArgsConstructor
 public class CommentController {
+
+    private final CommentService commentService;
+
+    @PostMapping("/{post_id}/comments")
+    public ResponseEntity<?> createComment(
+            @RequestParam("post_id") Long postId,
+            @RequestBody CreateCommentRequestDTO response,
+            @AuthenticationPrincipal CustomUserDetails userDetails
+    ) {
+        Long member_id=userDetails.getId();
+        CreateCommentResponseDTO result = commentService.createComment(postId, response, member_id);
+        return ResponseEntity.ok(Response.success("댓글 생성 성공",result));
+    }
 }
--- src/main/java/com/req2res/actionarybe/domain/post/repository/PostImageRepository.java
+++ src/main/java/com/req2res/actionarybe/domain/post/repository/PostImageRepository.java
@@ -2,7 +2,9 @@ package com.req2res.actionarybe.domain.post.repository;
 
 import com.req2res.actionarybe.domain.post.entity.PostImage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PostImageRepository extends JpaRepository<PostImage,Long> {
 
 }
