5522f976ff7f40795ff89bc13b6e726029d6aea9
dahyeon1216 <sally1023p@gmail.com>
2026-01-03 22:29:52 +0900

    fix:#9-StudyTimeManual import문 수정

--- src/main/java/com/req2res/actionarybe/domain/studyTime/repository/StudyTimeManualRepository.java
+++ src/main/java/com/req2res/actionarybe/domain/studyTime/repository/StudyTimeManualRepository.java
@@ -1,6 +1,7 @@
 package com.req2res.actionarybe.domain.studyTime.repository;
 
-import com.req2res.actionarybe.domain.point.entity.StudyTimeManual;
+
+import com.req2res.actionarybe.domain.studyTime.entity.StudyTimeManual;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
