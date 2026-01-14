f2dc25f66316d655a2e5a0db06d98a5c8d8b203a
sujeongmoon <msj0844@sookmyung.ac.kr>
2026-01-14 17:08:58 +0900

    fix: #94 - Chatting 삭제

--- src/main/java/com/req2res/actionarybe/domain/study/repository/ChattingRepository.java
+++
@@ -1,8 +0,0 @@
-package com.req2res.actionarybe.domain.study.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.req2res.actionarybe.domain.study.entity.Chatting;
-
-public interface ChattingRepository extends JpaRepository<Chatting, Long> {
-}
