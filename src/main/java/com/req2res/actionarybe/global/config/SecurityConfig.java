480889a8ec8e0424fd5a7a5d59fb2f5b26faa2d8
NaHyeon647 <forzeu@sookmyung.ac.kr>
2026-01-15 23:05:28 +0900

    fix: #105 - 프론트 주소 5173으로 변경

--- src/main/java/com/req2res/actionarybe/global/config/SecurityConfig.java
+++ src/main/java/com/req2res/actionarybe/global/config/SecurityConfig.java
@@ -65,7 +65,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000")); // 프론트 주소
+        config.setAllowedOrigins(List.of("http://13.209.205.33:5173")); // 프론트 주소
         config.setAllowedMethods(List.of("*"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
