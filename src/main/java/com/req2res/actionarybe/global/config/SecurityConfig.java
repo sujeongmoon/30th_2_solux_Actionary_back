c81e6363cbb88657cad13657b68e650fba47c5af
NaHyeon647 <forzeu@sookmyung.ac.kr>
2026-01-16 00:07:59 +0900

    fix: #105 - CORS 함수 명시적 사용

--- src/main/java/com/req2res/actionarybe/global/config/SecurityConfig.java
+++ src/main/java/com/req2res/actionarybe/global/config/SecurityConfig.java
@@ -33,6 +33,7 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http
+            .cors(cors -> cors.configurationSource(corsConfigurationSource()))
 			.csrf(csrf -> csrf.disable())
 			.sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 			.authorizeHttpRequests(auth -> auth
