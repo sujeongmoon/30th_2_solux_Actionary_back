6332d333e1b164f3356607c60026885f54310a37
NaHyeon647 <forzeu@sookmyung.ac.kr>
2026-01-16 14:32:04 +0900

    fix: #112 - id를 memberId로 수정 (LoginMemberResponseDTO)

--- src/main/java/com/req2res/actionarybe/domain/member/dto/LoginMemberResponseDTO.java
+++ src/main/java/com/req2res/actionarybe/domain/member/dto/LoginMemberResponseDTO.java
@@ -8,7 +8,7 @@ import java.time.LocalDate;
 @Getter
 @AllArgsConstructor
 public class LoginMemberResponseDTO {
-    Long id;
+    Long memberId;
     String profileImageUrl;
     String nickname;
     String phoneNumber;
