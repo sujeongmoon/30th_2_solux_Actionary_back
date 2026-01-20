d7b0d3b5e060bc305a108b218492cff24449bbf5
dahyeon1216 <sally1023p@gmail.com>
2026-01-20 22:04:55 +0900

    feat:#153-공부량 포인트 적립 API 내 스케줄러 설정

---
+++ src/main/java/com/req2res/actionarybe/domain/point/scheduler/StudyTimePointScheduler.java
@@ -0,0 +1,51 @@
+package com.req2res.actionarybe.domain.point.scheduler;
+
+import com.req2res.actionarybe.domain.member.entity.Member;
+import com.req2res.actionarybe.domain.member.repository.MemberRepository;
+import com.req2res.actionarybe.domain.point.service.PointService;
+import com.req2res.actionarybe.global.exception.CustomException;
+import com.req2res.actionarybe.global.exception.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class StudyTimePointScheduler {
+
+    private final PointService pointService;
+    private final MemberRepository memberRepository;
+
+    // 매일 00:00 실행
+    //테스트용으로 1분마다 실행
+    @Scheduled(cron = "0 * * * * *")
+    public void earnDailyStudyTimePoints() {
+
+        log.info("[Scheduler] Daily study-time point job started");
+
+        List<Member> members = memberRepository.findAll();
+
+        for (Member member : members) {
+            try {
+                pointService.earnStudyTimePoint(member.getId());
+            } catch (CustomException e) {
+
+                // 정상 스킵 케이스
+                if (e.getErrorCode() == ErrorCode.STUDY_TIME_POINT_ALREADY_EARNED_TODAY ||
+                        e.getMessage().contains("공부시간")) {
+
+                    log.info("[Scheduler] skip userId={} reason={}",
+                            member.getId(), e.getErrorCode());
+                    continue;
+                }
+            }
+        }
+
+        log.info("[Scheduler] Daily study-time point job finished");
+    }
+}
+
