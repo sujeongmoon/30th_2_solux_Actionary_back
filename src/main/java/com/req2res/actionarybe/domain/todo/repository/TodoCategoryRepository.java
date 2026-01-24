36889187356cd0d4f8ce49459aa33cacb5d7fc2e
dahyeon1216 <sally1023p@gmail.com>
2026-01-24 14:20:28 +0900

    fix:#192-투두 카테고리 생성, 목록 조회 API에 startDate 필드 추가

--- src/main/java/com/req2res/actionarybe/domain/todo/repository/TodoCategoryRepository.java
+++ src/main/java/com/req2res/actionarybe/domain/todo/repository/TodoCategoryRepository.java
@@ -3,6 +3,7 @@ package com.req2res.actionarybe.domain.todo.repository;
 import com.req2res.actionarybe.domain.todo.entity.TodoCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,9 +18,8 @@ public interface TodoCategoryRepository extends JpaRepository<TodoCategory, Long
     //유저 소유 + 존재 체크 메서드
     boolean existsByIdAndUserId(Long id, Long userId);
 
-    //특정 사용자로 최신순으로 카테고리 찾는 메소드
-    List<TodoCategory> findAllByUserIdOrderByCreatedAtAsc(Long userId);
-
+    //특정 사용자로 startDate 기준으로 카테고리 찾는 메소드
+    List<TodoCategory> findAllByUserIdAndStartDateLessThanEqualOrderByStartDateAsc(Long userId, LocalDate date);
 
 }
 
